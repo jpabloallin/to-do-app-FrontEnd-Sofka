ccc72f69c361789f94ef7d857d9dd9e2175f653c
jpabloallin <jpac.647@gmail.com>
2022-05-02 17:27:59 -0500

    Reducer.jsx --> Add-note case

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import StoreProvider from './components/StoreProvider'
 import ListOfToDo from './components/ListOfToDo'
+import Form from './components/Form'
 
 function App() {
 
   return (
     <StoreProvider>
       <h1>Hello from my app</h1>
+      <Form/>
       <ListOfToDo/>
     </StoreProvider>
   )
